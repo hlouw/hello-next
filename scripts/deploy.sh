b12b4ca986110d980c89c4555a615dec282d7f0b
Hentie Louw <hentie.louw@gmail.com>
2018-03-07 23:13:39 +0000

    Remove some steps from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,6 @@
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
 
-gcloud --quiet components update
 gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-gcloud config set project $GCLOUD_PROJECT
 
 gcloud gcloud app deploy --project $GCLOUD_PROJECT
